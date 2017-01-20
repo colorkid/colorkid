55d0de9be5ae3eb9852d955ed1e7c92b9b1e3b5b
Yuriy Musatov <colorkid@yandex.ru>
2017-01-20 07:50:29 +0300

    first

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var browserSync = require('browser-sync');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('less', function () {
+  return gulp.src('css/style.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+     }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', ['browserSync', 'less'], function (){
+  gulp.watch('css/*.less', ['less']);
+  gulp.watch('index.html');
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: ''
+    },
+  })
+});
+
